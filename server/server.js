da3169e3481e76fc28c50e9543a2db297b5e8034
Wilmer Contreras <wraulcontreras@gmail.com>
2019-09-08 17:33:04 -0500

    configuracion de error en el server.js

--- server/server.js
+++ server/server.js
@@ -18,9 +18,17 @@ app.get('/usuario', function(req, res) {
 
 app.post('/usuario', function(req, res) {
     let body = req.body;
-    res.json({
-        persona: body
-    });
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "El nombre es necesario"
+        });
+    } else {
+        res.json({
+            persona: body
+        });
+    }
 });
 
 app.put('/usuario/:id', function(req, res) {
