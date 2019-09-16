dd51bc0b3050c3345586fc8c8cb589b772d0252c
Wilmer Contreras <wraulcontreras@gmail.com>
2019-09-15 19:12:47 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://wilconr:Encr1pty0n@cluster0-dst6k.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
