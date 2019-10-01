16967a699fe8048c4ea41080365720a936158f6f
Wilmer Contreras <wraulcontreras@gmail.com>
2019-09-30 23:37:47 -0500

    Subida de archivos

--- server/config/config.js
+++ server/config/config.js
@@ -8,8 +8,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // -------Vencimiento del Token------------
 
-// 60 segundos * 60 minutos * 24 horas * 30 dias
-
 process.env.CADUCIDAD_TOKEN = '48h';
 
 // -------SEED de autenticación------------
